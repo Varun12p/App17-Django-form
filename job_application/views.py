2e3c305d62287fc7fa8d04b6bcb31e21e047280b
Varun <panjwanivarun12@gmail.com>
2024-10-02 14:28:54 -0400

    added email function

--- job_application/views.py
+++ job_application/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from job_application.forms import ApplicationForm
-
+from .models import Form
+from django.contrib import messages
+from django.core.mail import EmailMessage
 
 def index(request):
     if request.method == "POST":
@@ -11,4 +13,13 @@ def index(request):
             email = form.cleaned_data["email"]
             date = form.cleaned_data["date"]
             occupation = form.cleaned_data["occupation"]
+
+            Form.objects.create(first_name=first_name, last_name = last_name,
+                                email=email, date= date, occupation = occupation)
+
+            message_body = f"Your form was submitted. \n Thank You \n {first_name} {last_name}"
+            email_message = EmailMessage("Confirmation of Submission", message_body, to=[email])
+            email_message.send()
+
+            messages.success(request, "Form Submitted Successfully!")
     return render(request,"index.html")
